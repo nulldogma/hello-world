aaddac85a78f65ff7623e6075115d4e1bc2968cd
nulldogma <lrobles22@mail.pima.edu>
2024-09-10 18:33:04 -0700

    Update cis129_lab02_shortStory.py
    I added a short story of five sentences. This code will print the short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!")
+# Author: Lucas Robles
+# My first Python lab in my CIS129 course
+# Prints a simple short story about a farmer in a mine.
+
+print('A farmer goes down to the mines, looking for good ores for good tools. 
+Using a worn old ladder, she decends down a few levels, well armed and well supplied. 
+She was well and good, happy with her loot. 
+Too late she realizes she forgot the time, all those supplies and no watch, she falls asleep. 
+Wakes up in the morning safe and sound, with only one loot... a rescue bill')
